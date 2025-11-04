402373be9840b1e56841593257301b3687c3c4cb
SaviN7 <savinseven7@gmail.com>
2025-11-04 13:58:13 -0500

    basic player stuff

--- Menus/LevelLoad/level_load.gd
+++ Menus/LevelLoad/level_load.gd
@@ -1,24 +1,45 @@
 extends Node2D
 
+var shape_radius = 100
+var player_rotation = 0
+var player_x
+var player_y
+
+var preloaded_player = load("res://Menus/LevelLoad/LevelPlayer.tscn")
+var player = preloaded_player.instantiate()
+
+var song_name = Global.levels[Global.selected].name
+var bpm = Global.levels[Global.selected].bpm
+var events = Global.levels[Global.selected].events
+var sides = int(events[0].extraData.sides)
+
 func _ready() -> void:
-	var name = Global.levels[Global.selected].name
-	var bpm = Global.levels[Global.selected].bpm
-	var events = Global.levels[Global.selected].events
-	var sides = int(events[0].extraData.sides)
-	
 	create_shape(sides)
+	add_child(player)
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Esc"):
 		get_tree().change_scene_to_file("res://Menus/LevelMenu/LevelSelect.tscn")
 		
+	player_x = Global.screen_center_x + shape_radius * cos(deg_to_rad(player_rotation))
+	player_y = Global.screen_center_y + shape_radius * sin(deg_to_rad(player_rotation))
+	player.position = Vector2(player_x, player_y)
+	
+	if (Input.is_action_just_pressed("Left")):
+		player_rotation = rotation + (360/sides)
+		#if (rotation >= 360):
+			#rotation -= 360
+	else: if (Input.is_action_just_pressed("Right")):
+		player_rotation = rotation - (360/sides)
+		#if (rotation >= 360):
+			#rotation -= 360
+	
 func create_shape(vertices_amount):
-	var radius = 100
 	var vertices = PackedVector2Array()
 	for i in range(vertices_amount):
 		var angle = (i * 2 * PI) / vertices_amount
-		var x = radius * cos(angle)
-		var y = radius * sin(angle)
+		var x = shape_radius * cos(angle)
+		var y = shape_radius * sin(angle)
 		vertices.append(Vector2(x, y))
 		
 	var poly = Polygon2D.new()
---
+++ Menus/LevelLoad/level_player.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+func _draw() -> void:
+	draw_circle(Vector2(0, 0), 10, Color.WHITE)
+
+func _ready() -> void:
+	print("Player ready")
+	queue_redraw()
