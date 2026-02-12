8efb5623e4cdaeb9c3dc510eb4718e56a6fbc4ce
SaviN7 <savinseven7@gmail.com>
2026-02-12 11:40:41 -0500

    Update level_load.gd

--- Menus/LevelLoad/level_load.gd
+++ Menus/LevelLoad/level_load.gd
@@ -86,3 +86,6 @@ func create_shape(vertices_amount):
 	poly.color = Color(1, 1, 1)
 	poly.position = Vector2(Global.screen_center_x, Global.screen_center_y)
 	add_child(poly)	
+
+func die():
+	get_tree().change_scene_to_file("res://Menus/LevelMenu/LevelSelect.tscn")
